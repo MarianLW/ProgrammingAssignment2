72ec898ed18e604c6baaf48cb1c43eaadab77aed
Marian White <marianw123@aol.com>
2016-02-04 13:37:46 +0000

    committing from laptop

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## Put comments here that give an overall description of what your
+## This is my first change - testing out myuse of git.
 ## functions do
 
 ## Write a short comment describing this function
